f0f8805183cbc02eed974238e307c87cb784925f
nathanael <vieira_nathanael@hotmail.com>
2021-06-25 01:50:48 -0300

    first commit

---
+++ src/entities/Order.java
@@ -0,0 +1,37 @@
+package entities;
+
+import java.util.Date;
+
+import entities.enuns.OrderStatus;
+
+public class Order {
+	public Date moment;
+	public OrderStatus status;
+	
+	public Order() {
+	}
+
+	public Order(Date moment, OrderStatus status) {
+		this.moment = moment;
+		this.status = status;
+	}
+
+	public Date getMoment() {
+		return moment;
+	}
+
+	public void setMoment(Date moment) {
+		this.moment = moment;
+	}
+
+	public OrderStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(OrderStatus status) {
+		this.status = status;
+	}
+	
+	
+	
+}
