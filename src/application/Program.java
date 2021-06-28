1da6e50822506ed167c232f1d34f24664ba09e58
nathanael <vieira_nathanael@hotmail.com>
2021-06-28 00:02:52 -0300

    Acrescentando classe program.java

---
+++ src/application/Program.java
@@ -0,0 +1,28 @@
+package application;
+
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Name: ");
+		
+		System.out.print("E-mail: ");
+		System.out.print("Birth date (DD/MM/YYYY: ");
+		System.out.println("Enter order data:");
+		
+		System.out.print("Status: ");
+		System.out.print("How many items to this order? ");
+		
+		System.out.println("Enter #1 item data:");
+		System.out.print("Product name: ");
+		System.out.print("Quantity: ");
+
+		
+		
+		sc.close();
+	}
+
+}
